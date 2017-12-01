3e79923ea7b787b034c68285170720fdf56b1399
Tobias Reich <tobias.reich.ich@gmail.com>
2017-12-01 17:13:37 +0100

    Points

--- src/assets/components/browser/browser.data.js
+++ src/assets/components/browser/browser.data.js
@@ -3,5 +3,17 @@ module.exports = {
 	figure: {
 		src: '/assets/images/browser.png',
 		alternative: 'Browser'
-	}
+	},
+	points: [
+		{
+			x: 240,
+			y: 20,
+			delay: 0
+		},
+		{
+			x: 20,
+			y: 110,
+			delay: .5
+		}
+	]
 }
