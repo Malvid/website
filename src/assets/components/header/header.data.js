bf462b8fbe37f6f6c9a76de225947f59c825ebda
Tobias Reich <tobias.reich.ich@gmail.com>
2017-12-01 13:49:54 +0100

    Logo, templates etc.

---
+++ src/assets/components/header/header.data.js
@@ -0,0 +1,4 @@
+module.exports = {
+	logo: require('../logo/logo.data'),
+	nav: require('../nav/nav.data')
+}
---
+++ src/assets/components/index/index.data.js
@@ -0,0 +1,4 @@
+module.exports = {
+	header: require('../header/header.data'),
+	section_hero: require('../section_hero/section_hero.data')
+}
