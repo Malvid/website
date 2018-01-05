8a5bd6bcaba383053f1eb984e4d489f40cf83528
Tobias Reich <tobias.reich.ich@gmail.com>
2018-01-05 11:53:11 +0100

    Flap in hero

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,1 +1,22 @@
+import jump from 'jump.js'
+
 import './_parallax'
+
+document.querySelectorAll('a[href^="#"]').forEach((elem) => {
+
+	elem.onclick = (e) => {
+
+		const href = elem.getAttribute('href')
+		const target = document.querySelector(href)
+
+		jump(target, {
+			duration: 500,
+			a11y: true
+		})
+
+		e.preventDefault()
+		e.stopPropagation()
+
+	}
+
+})
