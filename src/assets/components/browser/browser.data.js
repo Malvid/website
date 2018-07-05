37f10826bf51c03aef4d1962cd4f7c56150873a3
Tobias Reich <tobias.reich.ich@gmail.com>
2018-07-05 18:01:48 +0200

    Browser, terminal and window

---
+++ src/assets/components/browser/browser.data.js
@@ -0,0 +1,29 @@
+module.exports = {
+	url: 'http://localhost:3000/',
+	figure: {
+		src: '/assets/images/browser.png',
+		width: 1300,
+		height: 688,
+		alternative: 'Browser'
+	},
+	points: [
+		{
+			x: 240,
+			y: 20,
+			delay: 0,
+			tooltip: {
+				position: 'top',
+				bodytext: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'
+			}
+		},
+		{
+			x: 20,
+			y: 110,
+			delay: .5,
+			tooltip: {
+				position: 'right',
+				bodytext: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'
+			}
+		}
+	]
+}
---
+++ src/assets/components/terminal/terminal.data.js
@@ -0,0 +1,14 @@
+module.exports = {
+	bodytext: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>',
+	points: [
+		{
+			x: 140,
+			y: 20,
+			delay: 0,
+			tooltip: {
+				position: 'top',
+				bodytext: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.'
+			}
+		}
+	]
+}
