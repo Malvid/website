cb119a02cf78cfb73eae88f5592386221d497c8c
Tobias Reich <tobias.reich.ich@gmail.com>
2019-02-13 13:12:48 +0100

    Fix example data

--- src/assets/components/feature/feature.data.js
+++ src/assets/components/feature/feature.data.js
@@ -1,5 +1,5 @@
 module.exports = {
-	icon: 'bug',
+	icon: 'flash',
 	header: 'Lorem ipsum',
 	bodytext: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>'
 }
